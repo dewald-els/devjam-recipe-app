6ddf06d3ce6f2863e21417b481e0e6481c0aafe1
Dewald Els <dewald.els@noroff.no>
2022-01-05 20:47:33 +0100

    FIX: Add static assets for express serve

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(requireHTTPS);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static("./dist/recipe-app"));
 
 app.use((req, res, next) => {
 	if (!API_KEY) {
